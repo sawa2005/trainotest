36e3a87600114c960d8a5b5553e976bd3eaf1e11
Samuel Ward <samuelleeward9@gmail.com>
2024-07-31 15:45:28 +0200

    user info rendered

--- trainotest/app/user/[id]/page.js
+++ trainotest/app/user/[id]/page.js
@@ -1,6 +1,8 @@
-export function generateStaticParams() {
+import Image from 'next/image'
+
+/* export function generateStaticParams() {
     return [{ id: '175' }]
-  }
+} */
 
   // - /user/175
   
@@ -9,5 +11,20 @@ export function generateStaticParams() {
     
     const user = await fetch(`https://traino.nu/php/testgetuser.php?id=${id}`).then((res) => res.json())
 
-    console.log(user);
-  }
+    return (
+        <div className="flex justify-between size-3/4 bg-white/50 items-center rounded-lg p-6 gap-4 max-w-2xl">
+            <div>
+                <h1 className='text-2xl font-semibold'>{user.firstname} {user.lastname}</h1>
+                <p>{user.age} år</p><br />
+                <p>{user.user_about}</p>
+            </div>
+            <Image
+                src={user.thumbnail}
+                width={250}
+                height={250}
+                alt="Picture of the user"
+                className='rounded-lg'
+            />
+        </div>
+    )
+}
